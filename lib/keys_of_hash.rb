29fd1295527770808fc712054d7d8c303ca1b62c
Melissa Nock <melnock@gmail.com>
2018-01-23 02:54:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,13 +2,13 @@ class Hash
   def keys_of(*arguments)
     arr = []
    
-   arguments.each {|i|
+  
     self.each {|k, v| 
       if v == arguments.each
         arr << k
       end
     }
-    } 
+    
 
     arr
   end
